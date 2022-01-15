dcbe43a751c535db816a1187dc56a0d151b5d66d
mukappalambda <maokailan24@gmail.com>
2022-01-15 21:28:02 +0800

    tweak error importing

--- gorm-sqlite/crud.go
+++ gorm-sqlite/crud.go
@@ -8,33 +8,33 @@ import (
 )
 
 type User struct {
-	gorm.Model
-	UserId int    `json:"user_id,omitempty"`
-	Name   string `json:"name,omitempty"`
+  gorm.Model
+  UserId int    `json:"user_id,omitempty"`
+  Name   string `json:"name,omitempty"`
 }
 
 func main()  {
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
-	if err != nil {
-		panic("Failed to connect database")
-	}
+  db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+  if err != nil {
+    panic("Failed to connect database")
+  }
 
-	fmt.Println("Create ...")
-	db.AutoMigrate(&User{})
-	db.Create(&User{UserId: 1, Name: "alex"})
+  fmt.Println("Create ...")
+  db.AutoMigrate(&User{})
+  db.Create(&User{UserId: 1, Name: "alex"})
 
-	fmt.Println("Read ...")
-	var user User
-	db.First(&user, 1)
-	fmt.Println(user)
+  fmt.Println("Read ...")
+  var user User
+  db.First(&user, 1)
+  fmt.Println(user)
 
-	// fmt.Println("Update ...")
-	// db.Model(&user).Where("Name = ?", "alex").Update("Name", "bob")
-	// db.First(&user, 1)
-	// fmt.Println(user)
+  // fmt.Println("Update ...")
+  // db.Model(&user).Where("Name = ?", "alex").Update("Name", "bob")
+  // db.First(&user, 1)
+  // fmt.Println(user)
 
-	fmt.Println("Delete ...")
-	db.Where("Name = ?", "bob").Delete(&user)
-	fmt.Println(user)
-	
+  fmt.Println("Delete ...")
+  db.Where("Name = ?", "bob").Delete(&user)
+  fmt.Println(user)
+  
 }
