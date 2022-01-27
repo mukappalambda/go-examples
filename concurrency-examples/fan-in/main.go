f3925d19e70324263974fe864dca18d7be188ce9
mukappalambda <maokailan24@gmail.com>
2022-01-27 21:39:00 +0800

    add the fan-in pattern

---
+++ concurrency-examples/fan-in/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main()  {
+	c1 := producer("alex", 1)
+	c2 := producer("bob", 2)
+	c3 := producer("mark", 3)
+	c := fanIn(c1, c2, c3)
+
+	for v := range c {
+		fmt.Println(v)
+	}
+	
+}
+
+func producer(name string, count int) <-chan string {
+	c := make(chan string)
+	go func() {
+		defer close(c)
+		for i := 0; i < count; i++ {
+			time.Sleep(time.Second)
+			c <- name
+		}
+	}()
+
+	return c
+}
+
+func fanIn(cs ...<-chan string) <-chan string {
+	c := make(chan string)
+	go func() {
+		defer close(c)
+		for _, ci := range cs {
+			for v := range ci {
+				c <- v
+			}
+		}
+	}()
+
+	return c
+}
