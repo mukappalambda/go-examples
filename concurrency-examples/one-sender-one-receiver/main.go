29190f97ada9c73a3420f74354ab629b3632adc4
mukappalambda <maokailan24@gmail.com>
2022-01-27 21:12:59 +0800

    add one producer and one consumer pattern

---
+++ concurrency-examples/one-sender-one-receiver/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main()  {
+	var wg sync.WaitGroup
+
+	done := make(chan struct{})
+
+	c := sender(done)
+	
+	wg.Add(1)
+	go func (ch <-chan string) {
+		defer wg.Done()
+
+		for i := 0; i < 5; i++ {
+			fmt.Println(<-ch)
+			time.Sleep(time.Second)
+		}
+	}(c)
+	wg.Wait()
+	fmt.Println("Closing the channel to cancel the sender.")
+	close(done)
+	fmt.Println(<-c)
+	
+}
+
+func sender(done <-chan struct{}) <-chan string {
+	c := make(chan string)
+	go func ()  {
+		defer close(c)
+		for {
+			select {
+			case <-done:
+				c <- "sender: bye!"
+				return
+			case c <- "sender pings!":
+			}
+		}
+	}()
+	return c
+}
