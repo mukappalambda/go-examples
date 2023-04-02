be7d75eab484db1662a1fa8c4ee7a8531b8b02ef
mukappalambda <maokailan24@gmail.com>
2023-04-02 22:12:40 +0800

    chore: Add the example of unmarshalling JSON data

---
+++ json-processing/parse_json_without_knowing_the_schema.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+func main() {
+	jsonData := []byte(`{
+  "userId": 1,
+  "id": 1,
+  "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+  "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+}`)
+	var data map[string]interface{}
+	err := json.Unmarshal(jsonData, &data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(data)
+}
