a551ae4d111733f1986038b7d70cc34ef6410e15
mukappalambda <maokailan24@gmail.com>
2024-02-25 16:23:31 +0800

    chore(interface): add interface-tricks (#38)

---
+++ interface-tricks/go.mod
@@ -0,0 +1,3 @@
+module example.com/interface-tricks
+
+go 1.22.0
---
+++ interface-tricks/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+type Ingester interface {
+	Ingest([]float64) []float64
+}
+
+type IngesterFunc func([]float64) []float64
+
+func (i IngesterFunc) Ingest(data []float64) []float64 {
+	return i(data)
+}
+
+func addOne(data []float64) []float64 {
+	out := make([]float64, len(data))
+	for k, v := range data {
+		out[k] = v + 1
+	}
+	return out
+}
+
+func ScaledTransform(i Ingester, scale float64) Ingester {
+	return IngesterFunc(func(data []float64) []float64 {
+		out := make([]float64, len(data))
+		for k, v := range i.Ingest(data) {
+			out[k] = scale * v
+		}
+		return out
+	})
+}
+
+var (
+	scale = flag.Float64("scale", 1.23, "scale coefficient")
+)
+
+func main() {
+	flag.Parse()
+	data := make([]float64, 5)
+
+	fmt.Printf("initial data: %v\n", data)
+	inputIngester := IngesterFunc(addOne)
+	fmt.Printf("data by addOne: %v\n", inputIngester.Ingest(data))
+	outputIngester := ScaledTransform(inputIngester, *scale)
+	fmt.Printf("data by addOne then scaled by %f: %v\n", *scale, outputIngester.Ingest(data))
+}
