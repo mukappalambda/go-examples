a4d63fd0c9df65e4f6d1edfd94fad2f233f34010
mukappalambda <maokailan24@gmail.com>
2024-05-04 11:19:50 +0800

    chore(http): timeout example (#47)

---
+++ http-gym/timeout/go.mod
@@ -0,0 +1,3 @@
+module http-gym/timeout
+
+go 1.22.2
---
+++ http-gym/timeout/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	h := handler(500 * time.Millisecond)
+	handlerWithTimeout := http.TimeoutHandler(h, 300*time.Millisecond, "timeout is reached")
+
+	// curl http://localhost:8080/notimeout => 200 OK
+	http.HandleFunc("GET /notimeout", h)
+	// curl http://localhost:8080/withtimeout => 503 Service Unavailable
+	http.Handle("GET /withtimeout", handlerWithTimeout)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil && err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+}
+
+func handler(d time.Duration) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(d)
+		fmt.Fprint(w, "root")
+	}
+}
