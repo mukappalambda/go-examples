f28989e419b962e173ec67d451d7b305e38b6d7d
mukappalambda <maokailan24@gmail.com>
2024-05-04 19:31:32 +0800

    chore(http-gym): add http middleware (#47)

---
+++ http-gym/middleware/go.mod
@@ -0,0 +1,3 @@
+module http-gym/middleware
+
+go 1.22.2
---
+++ http-gym/middleware/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	handler := http.DefaultServeMux
+	handler.HandleFunc("GET /", root())
+	handler.Handle("GET /verbose", LoggerHandler(root()))
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: handler,
+	}
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+}
+
+func LoggerHandler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s - %s - %s", r.Method, r.Host, r.Header)
+		h.ServeHTTP(w, r)
+	})
+}
+
+func root() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "root")
+	}
+}
