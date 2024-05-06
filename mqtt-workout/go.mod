a8c760269a4319eb7cc038070f00ed909b65b16c
mukappalambda <maokailan24@gmail.com>
2024-05-07 00:25:13 +0800

    chore(mqtt): talk with mosquitto (#49)

---
+++ mqtt-workout/go.mod
@@ -0,0 +1,10 @@
+module example.com/mqtt-workout
+
+go 1.22.2
+
+require (
+	github.com/eclipse/paho.mqtt.golang v1.4.3 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+)
---
+++ mqtt-workout/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/eclipse/paho.mqtt.golang"
+)
+
+var (
+	topic    = flag.String("topic", "my-topic", "topic name")
+	scheme   = flag.String("scheme", "tcp", "scheme")
+	host     = flag.String("host", "localhost", "host")
+	port     = flag.String("port", "1883", "port")
+	clientId = flag.String("clientId", "my-client", "client id")
+	payload  = flag.String("payload", "hello world", "payload")
+)
+
+func main() {
+	flag.Parse()
+
+	server := fmt.Sprintf("%s://%s:%s", *scheme, *host, *port)
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(server).SetClientID(*clientId)
+	opts.SetOnConnectHandler(onConn)
+	opts.SetDefaultPublishHandler(func(client mqtt.Client, msg mqtt.Message) {
+		fmt.Printf("%v - Topic: %s; payload: %s", time.Now().Truncate(time.Microsecond), msg.Topic(), msg.Payload())
+	})
+	client := mqtt.NewClient(opts)
+
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		log.Fatal(token.Error())
+	}
+
+	if token := client.Subscribe(*topic, 0, nil); token.Wait() && token.Error() != nil {
+		log.Fatal(token.Error())
+	}
+
+	go func() {
+		token := client.Publish(*topic, 0, false, *payload)
+		token.Wait()
+	}()
+
+	time.Sleep(250 * time.Millisecond)
+	client.Disconnect(250)
+}
+
+func onConn(client mqtt.Client) {
+	fmt.Printf("%v - Client connected\n", time.Now().Truncate(time.Microsecond))
+}
