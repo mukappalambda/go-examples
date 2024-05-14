5855d1adcce60906a86ebee8ae2848932150f010
mukappalambda <maokailan24@gmail.com>
2024-05-14 11:54:09 +0800

    refactor(smtp): better error handling

--- smtp-example/main.go
+++ smtp-example/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"flag"
+	"fmt"
 	"log"
 	"net/smtp"
 	"os"
@@ -10,7 +12,7 @@ import (
 
 var (
 	username = flag.String("username", "", "username")
-	pass     = flag.String("pass", "./.password", "path to password")
+	pass     = flag.String("pass", ".password", "path to password")
 	host     = flag.String("host", "smtp.gmail.com", "host")
 	addr     = flag.String("addr", "smtp.gmail.com:587", "server addr")
 	from     = flag.String("from", "", "from")
@@ -18,31 +20,28 @@ var (
 )
 
 func main() {
-	if err := run(); err != nil {
+	flag.Parse()
+	if err := run(*pass, *username, *host, *addr, *from, *to); err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			fmt.Printf("file does not exist: %q\n", *pass)
+			os.Exit(1)
+		}
 		log.Fatal(err)
 	}
 }
 
-func run() error {
-	flag.Parse()
-	byt, err := os.ReadFile(*pass)
+func run(name string, username string, host string, addr string, from string, to string) error {
+	byt, err := os.ReadFile(name)
 	if err != nil {
-		return err
+		return fmt.Errorf("error reading file: %q: %w", *pass, err)
 	}
+	password := string(byt)
 	msg := []byte("Subject: Introduction to golang\r\n" + "This is the message of the mail.\r\n")
 
-	err = sendMail(*username, string(byt), *host, *addr, *from, strings.Split(*to, " "), msg)
+	auth := smtp.PlainAuth("" /* identity */, username /* username */, password /* password */, host /* host */)
+	err = smtp.SendMail(addr /* addr */, auth /* auth */, from /* from */, strings.Split(to, " ") /* to */, msg /* msg */)
 	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func sendMail(username string, password string, host string, addr string, from string, to []string, msg []byte) error {
-	auth := smtp.PlainAuth("", username, password, host)
-	err := smtp.SendMail(addr, auth, from, to, msg)
-	if err != nil {
-		return err
+		return fmt.Errorf("error sending mail: %q: %w", addr, err)
 	}
 	return nil
 }
