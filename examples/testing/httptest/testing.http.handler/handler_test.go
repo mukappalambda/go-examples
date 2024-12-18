30cce09f70cc30bf20e670706e0385869a3ae39c
Mao-Kai Lan <45162039+mukappalambda@users.noreply.github.com>
2024-12-18 08:46:57 +0800

    ✨ chore(httptest): add the example of testing a HTTP handler (#89) (#90)

---
+++ examples/testing/httptest/testing.http.handler/handler_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetUsers(t *testing.T) {
+	ts := httptest.NewServer(GetUsers())
+	defer ts.Close()
+	client := ts.Client()
+	res, _ := client.Get(fmt.Sprintf("%s", ts.URL))
+	if res.StatusCode != http.StatusOK {
+		t.Fatalf("want %d; got %d\n", http.StatusOK, res.StatusCode)
+	}
+	var want string
+	want = "my-value"
+	got := res.Header.Get("X-Custom-Header")
+	if got != want {
+		t.Fatalf("want: %q; got %q\n", want, got)
+	}
+	want = "get all users"
+	buf, _ := io.ReadAll(res.Body)
+	if !bytes.Equal(buf, []byte(want)) {
+		t.Fatalf("want: %s; got: %s\n", want, string(buf))
+	}
+}
