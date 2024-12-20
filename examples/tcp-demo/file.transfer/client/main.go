29f30143a3060d24027e70c0dc17478026691a65
Mao-Kai Lan <45162039+mukappalambda@users.noreply.github.com>
2024-12-20 09:31:29 +0800

    🥅 fix(tcp-demo): better error handling on server and client (#98)

--- examples/tcp-demo/file.transfer/client/main.go
+++ examples/tcp-demo/file.transfer/client/main.go
@@ -12,21 +12,29 @@ func main() {
 	port := 8080
 	conn, err := net.Dial("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("error connecting to the network: %s\n", err)
 	}
+	log.Printf("connected to the network successfully")
 	defer conn.Close()
 	dir, err := os.Getwd()
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("error returning working directory: %s\n", err)
+		conn.Close()
+		os.Exit(1)
 	}
 	fileName := fmt.Sprintf("%s/my_file.txt", dir)
 	f, err := os.Open(fileName)
 	if err != nil {
-		log.Fatalf("failed to open file: %s\n", err)
+		log.Printf("error opening file: %s\n", err)
+		conn.Close()
+		os.Exit(1)
 	}
-	_, err = io.Copy(conn, f)
+	n, err := io.Copy(conn, f)
 	if err != nil {
-		log.Fatalf("failed to send file to tcp connection: %v\n", err)
+		log.Printf("failed to send file to tcp connection: %v\n", err)
+		f.Close()
+		conn.Close()
+		os.Exit(1)
 	}
-	fmt.Println("sent file content successfully.")
+	fmt.Printf("sent file content successfully: %d bytes sent\n", n)
 }
--- examples/tcp-demo/file.transfer/server/main.go
+++ examples/tcp-demo/file.transfer/server/main.go
@@ -12,14 +12,14 @@ func main() {
 	port := 8080
 	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("error listening at %d\n", port)
 	}
 	fmt.Printf("server is listening at %s\n", ln.Addr().String())
 	defer ln.Close()
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			log.Fatalf("failed to accept tcp connection: %s\n", err)
+			log.Printf("error accepting a new connection: %s\n", err)
 		}
 		go handleConnection(conn)
 	}
@@ -27,18 +27,27 @@ func main() {
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
+	remoteAddr := conn.RemoteAddr().String()
+	log.Printf("client@%s is connected\n", remoteAddr)
 	dir, err := os.Getwd()
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("error returning working directory: %s\n", err)
+		conn.Close()
+		return
 	}
 	fileName := fmt.Sprintf("%s/tmp_file.txt", dir)
 	f, err := os.Create(fileName)
 	if err != nil {
-		log.Fatalf("failed to create file: %s\n", fileName)
+		log.Printf("error creating a new file: %s\n", fileName)
+		conn.Close()
+		return
 	}
 	defer f.Close()
-	_, err = io.Copy(f, conn)
+	n, err := io.Copy(f, conn)
 	if err != nil {
-		log.Fatalf("failed to copy: %s\n", err)
+		log.Printf("error copying to another connection: %s\n", err)
+		f.Close()
+		conn.Close()
 	}
+	log.Printf("%s sent %d bytes successfully\n", remoteAddr, n)
 }
