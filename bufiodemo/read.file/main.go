fa1e494502831643c9b24a8e1de465d448cf0315
Mao-Kai Lan <45162039+mukappalambda@users.noreply.github.com>
2025-01-05 23:52:43 +0800

    🍻 chore(bufio): use both `bufio.NewScanner` and `bufio.NewReader` methods to read files (#57) (#146)

--- bufiodemo/read.file/main.go
+++ bufiodemo/read.file/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
@@ -13,9 +14,24 @@ func main() {
 		log.Fatal(err)
 	}
 	defer f.Close()
-	s := bufio.NewScanner(f)
-	var i int
-	for i = 0; s.Scan(); i++ {
-		fmt.Printf("%s\n", s.Bytes())
+	fmt.Println("Method 1: Read files using bufio.NewScanner()...")
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		fmt.Printf("%s\n", scanner.Bytes())
+	}
+	fmt.Println("Method 2: Read files using bufio.NewReader()...")
+	if _, err := f.Seek(0, io.SeekStart); err != nil {
+		log.Fatalf("error setting the offset: %s\n", err)
+	}
+	reader := bufio.NewReader(f)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatalf("error reading from file: %s\n", err)
+		}
+		fmt.Print(line)
 	}
 }
