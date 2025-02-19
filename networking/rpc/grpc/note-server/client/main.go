6dae60cfa50eae9e04a211f7c6ae1762aefa89a7
mukappalambda <maokailan24@gmail.com>
2025-02-19 10:16:07 +0800

    ✨ chore(rpc): ListNote invocation on client side

--- networking/rpc/grpc/note-server/client/main.go
+++ networking/rpc/grpc/note-server/client/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"time"
 
@@ -34,15 +35,48 @@ func main() {
 	}
 	defer conn.Close()
 	client := pb.NewNoteServiceClient(conn)
-	req := &pb.CreateNoteRequest{
-		Title:   "my-title",
-		Content: "my-content",
+	if err := createFakeNotes(client); err != nil {
+		log.Fatal(err)
 	}
+	if err := listNotes(client); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func listNotes(client pb.NoteServiceClient) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	stream, err := client.ListNotes(ctx, &pb.ListNotesRequest{Page: 0, PageSize: 0})
+	if err != nil {
+		return fmt.Errorf("failed to list notes: %s", err)
+	}
+	for {
+		note, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		fmt.Printf("received note: %+v\n", note)
+	}
+	return nil
+}
+
+func createFakeNotes(client pb.NoteServiceClient) error {
 	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
 	defer cancel()
-	res, err := client.CreateNote(ctx, req)
-	if err != nil {
-		log.Fatalf("error creating note: %v\n", err)
+	fakeNotes := []string{"alpha", "beta", "gamma", "delta"}
+	for _, fakeNote := range fakeNotes {
+		req := &pb.CreateNoteRequest{
+			Title:   fakeNote,
+			Content: fakeNote,
+		}
+		res, err := client.CreateNote(ctx, req)
+		if err != nil {
+			return fmt.Errorf("error creating note: %v", err)
+		}
+		fmt.Printf("response: %+v\n", res)
 	}
-	fmt.Printf("response: %+v\n", res)
+	return nil
 }
