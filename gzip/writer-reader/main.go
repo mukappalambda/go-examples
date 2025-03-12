45bb04710398c4fe0dcc6e3b9256890f02c57910
mukappalambda <maokailan24@gmail.com>
2025-03-12 08:08:16 +0800

    🚨 fix(gzip): fix linter warnings

--- gzip/writer-reader/main.go
+++ gzip/writer-reader/main.go
@@ -3,30 +3,40 @@ package main
 import (
 	"bytes"
 	"compress/gzip"
+	"fmt"
 	"io"
-	"log"
 	"os"
 )
 
 func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
 	var buf bytes.Buffer
 	zw := gzip.NewWriter(&buf)
+	var err error
 	defer func() {
-		if err := zw.Close(); err != nil {
-			log.Fatalf("error closing writer: %s\n", err)
-		}
+		err = zw.Close()
 	}()
+	if err != nil {
+		return fmt.Errorf("error closing writer: %w", err)
+	}
 	data := []byte(`gzip is a file format and a software application used for file compression and decompression. The program was created by Jean-loup Gailly and Mark Adler as a free software replacement for the compress program used in early Unix systems, and intended for use by GNU (from which the "g" of gzip is derived). Version 0.1 was first publicly released on 31 October 1992, and version 1.0 followed in February 1993.`)
 	if _, err := zw.Write(data); err != nil {
-		log.Fatalf("error writing data to writer: %s\n", err)
+		return fmt.Errorf("error writing data to writer: %w", err)
 	}
 	zw.Close()
 	zr, err := gzip.NewReader(&buf)
 	if err != nil {
-		log.Fatalf("error creating reader: %s\n", err)
+		return fmt.Errorf("error creating reader: %w", err)
 	}
 	defer zr.Close()
 	if _, err := io.CopyN(os.Stdout, zr, 32768); err != nil {
-		log.Fatalf("error copying to writer from reader: %s\n", err)
+		return fmt.Errorf("error copying to writer from reader: %w", err)
 	}
+	return nil
 }
