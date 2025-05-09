117ec3ff48ad11259bf3756cdbba6e8a8e20baa5
Mao-Kai Lan <45162039+mukappalambda@users.noreply.github.com>
2025-05-09 14:59:02 +0800

    ♻️ chore(note-server): create command package (#295)
    Signed-off-by: Mao-Kai Lan <maokailan24@gmail.com>

---
+++ networking/rpc/grpc/note-server/cmd/noted/command/command.go
@@ -0,0 +1,34 @@
+package command
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/mukappalambda/go-examples/networking/rpc/grpc/note_server/cmd/noted/server"
+)
+
+type app struct {
+}
+
+func App() *app {
+	return &app{}
+}
+
+func (a *app) Run() error {
+	port := 50051
+	addr := fmt.Sprintf("localhost:%d", port)
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		return fmt.Errorf("error listening on %s", addr)
+	}
+	defer ln.Close()
+	server, err := server.New()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("server running on %s\n", addr)
+	if err := server.Serve(ln); err != nil {
+		return fmt.Errorf("error serving: %v", err)
+	}
+	return nil
+}
--- networking/rpc/grpc/note-server/cmd/noted/main.go
+++ networking/rpc/grpc/note-server/cmd/noted/main.go
@@ -2,34 +2,15 @@ package main
 
 import (
 	"fmt"
-	"net"
 	"os"
 
-	"github.com/mukappalambda/go-examples/networking/rpc/grpc/note_server/cmd/noted/server"
+	"github.com/mukappalambda/go-examples/networking/rpc/grpc/note_server/cmd/noted/command"
 )
 
 func main() {
-	if err := run(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
+	app := command.App()
+	if err := app.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "note-server: %s\n", err)
 		os.Exit(1)
 	}
-}
-
-func run() error {
-	port := 50051
-	addr := fmt.Sprintf("localhost:%d", port)
-	ln, err := net.Listen("tcp", addr)
-	if err != nil {
-		return fmt.Errorf("error listening on %s", addr)
-	}
-	defer ln.Close()
-	server, err := server.New()
-	if err != nil {
-		return err
-	}
-	fmt.Printf("server running on %s\n", addr)
-	if err := server.Serve(ln); err != nil {
-		return fmt.Errorf("error serving: %v", err)
-	}
-	return nil
 }
