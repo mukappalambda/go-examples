46c2f976ddf7245483a432868035c8622ab173b1
Mao-Kai Lan <maokailan24@gmail.com>
2025-05-09 14:46:09 +0800

    ✨ chore(note-server): add server package
    Signed-off-by: Mao-Kai Lan <maokailan24@gmail.com>

--- networking/rpc/grpc/note-server/cmd/noted/main.go
+++ networking/rpc/grpc/note-server/cmd/noted/main.go
@@ -5,13 +5,7 @@ import (
 	"net"
 	"os"
 
-	pb "github.com/mukappalambda/go-examples/networking/rpc/grpc/note_server/api/services/notes"
-	"github.com/mukappalambda/go-examples/networking/rpc/grpc/note_server/cmd/noted/services"
-
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/health"
-	healthgrpc "google.golang.org/grpc/health/grpc_health_v1"
-	"google.golang.org/grpc/orca"
+	"github.com/mukappalambda/go-examples/networking/rpc/grpc/note_server/cmd/noted/server"
 )
 
 func main() {
@@ -29,15 +23,12 @@ func run() error {
 		return fmt.Errorf("error listening on %s", addr)
 	}
 	defer ln.Close()
-	serverOpts := []grpc.ServerOption{
-		orca.CallMetricsServerOption(nil),
-		grpc.UnaryInterceptor(services.StupidInterceptor),
+	server, err := server.New()
+	if err != nil {
+		return err
 	}
-	grpcServer := grpc.NewServer(serverOpts...)
-	healthgrpc.RegisterHealthServer(grpcServer, health.NewServer())
-	pb.RegisterNoteServiceServer(grpcServer, services.NewServer())
 	fmt.Printf("server running on %s\n", addr)
-	if err := grpcServer.Serve(ln); err != nil {
+	if err := server.Serve(ln); err != nil {
 		return fmt.Errorf("error serving: %v", err)
 	}
 	return nil
