6af16f2f6da231884965a1dfa7418172847dbfa6
Mao-Kai Lan <45162039+mukappalambda@users.noreply.github.com>
2025-06-02 09:00:29 +0800

    ♻️ chore(http): update example of custom transport (#306)
    Signed-off-by: Mao-Kai Lan <maokailan24@gmail.com>

--- web/http/http-client/custom-transport/main.go
+++ web/http/http-client/custom-transport/main.go
@@ -6,53 +6,67 @@ import (
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"time"
 )
 
-type myRoundTripper struct{}
+type MyRoundTripper struct {
+	*log.Logger
+}
 
-func (m *myRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
+func NewMyRoundTripper(logger *log.Logger) *MyRoundTripper {
+	return &MyRoundTripper{
+		Logger: logger,
+	}
+}
+
+func (m *MyRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
 	transport := http.DefaultTransport
 	start := time.Now()
 	res, err := transport.RoundTrip(r)
 	elapsed := time.Since(start)
-	log.Printf("method: %q; path: %q; elapsed time: %s\n", r.Method, r.URL.Path, elapsed)
+	m.Logger.Printf("method: %q; path: %q; elapsed time: %s\n", r.Method, r.URL.Path, elapsed)
 	return res, err
 }
 
 var (
-	_  http.RoundTripper = (*myRoundTripper)(nil)
+	_  http.RoundTripper = (*MyRoundTripper)(nil)
 	ts *httptest.Server
 )
 
 func main() {
-	http.HandleFunc("GET /fast", handleData(0))
-	http.HandleFunc("GET /slow", handleData(500*time.Millisecond))
-	ts = httptest.NewServer(nil)
 	if err := run(); err != nil {
-		ts.Close()
-		log.Fatal(err)
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
 	}
-	ts.Close()
 }
 
 func run() error {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /fast", handleData(0))
+	mux.HandleFunc("GET /slow", handleData(500*time.Millisecond))
+	ts = httptest.NewServer(mux)
+	defer ts.Close()
+	log.Printf("test server is running on %q\n", ts.URL)
 	ctx := context.Background()
 	if err := fetchHTTPResponse(ctx, "/fast"); err != nil {
+		ts.Close()
 		return err
 	}
 	if err := fetchHTTPResponse(ctx, "/slow"); err != nil {
+		ts.Close()
 		return err
 	}
 	return nil
 }
 
 func fetchHTTPResponse(ctx context.Context, path string) error {
-	client := NewClient()
+	client := DefaultClient()
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, ts.URL+path, nil)
 	if err != nil {
 		return fmt.Errorf("failed to create new request to %s: %s", path, err)
 	}
+	log.Printf("client is sending request: %s\n", req.URL)
 	res, err := client.Do(req)
 	if err != nil {
 		res.Body.Close()
@@ -62,11 +76,14 @@ func fetchHTTPResponse(ctx context.Context, path string) error {
 	return nil
 }
 
-func NewClient() *http.Client {
-	client := &http.Client{
-		Transport: &myRoundTripper{},
+func DefaultClient() *http.Client {
+	return NewClient(NewMyRoundTripper(log.Default()))
+}
+
+func NewClient(transport http.RoundTripper) *http.Client {
+	return &http.Client{
+		Transport: transport,
 	}
-	return client
 }
 
 func handleData(delay time.Duration) http.HandlerFunc {
